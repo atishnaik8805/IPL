a790fc090002059f5d61d3255b4f35f4c81bd045
atish_naik <atishnaik8805@gmail.com>
2021-06-26 15:38:29 +0530

    DB fix

--- my_app/admin.py
+++ my_app/admin.py
@@ -13,5 +13,5 @@ def run():
         #print(row)
         #id,season,city,date,team1,team2,toss_winner,toss_decision,result,dl_applied,winner,win_by_runs,win_by_wickets,player_of_match,venue,umpire1,umpire2,umpire3
         IPL.objects.get_or_create(season=row[1],city=row[2],date=row[3],team1=row[4],team2=row[5],toss_winner=row[6],toss_decision=row[7],result=row[8],dl_applied=row[9],winner=row[10],win_by_runs=row[11],win_by_wickets=row[12],player_of_match=row[13],venue=row[14],umpire1=row[15],umpire2=row[16],umpire3=row[17])
-run()
 admin.site.register(IPL)
+run()
